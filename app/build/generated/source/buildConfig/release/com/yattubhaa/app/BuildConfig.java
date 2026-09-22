f61ecb08f0d1d99ac0413b53253f59999985325f
Yashwant Gurbani <yashwantgurbani@Yashwants-MacBook-Pro.local>
2026-09-22 02:16:54 +0100

    full build added

---
+++ app/build/generated/source/buildConfig/release/com/yattubhaa/app/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.yattubhaa.app;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.yattubhaa.app";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "0.1.0";
+  // Field from build type: release
+  public static final String DEFAULT_RELAY_URL = "";
+}
