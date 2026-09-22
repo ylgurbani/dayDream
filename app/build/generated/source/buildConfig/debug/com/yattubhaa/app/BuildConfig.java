43866e6fadfde04cace21e882a003e7f6979f5f0
ylgurbani <71453387+ylgurbani@users.noreply.github.com>
2026-09-22 01:07:09 +0100

    Add files via upload
    app build generated

---
+++ app/build/generated/source/buildConfig/debug/com/yattubhaa/app/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.yattubhaa.app;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.yattubhaa.app";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "0.1.0";
+  // Field from build type: debug
+  public static final String DEFAULT_RELAY_URL = "ws://10.0.2.2:8787";
+}
